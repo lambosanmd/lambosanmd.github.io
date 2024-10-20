eb5813b71daf4fe7aeb88e8e18bc2d227133496e
Anderson Jonas <lambosanmd@gmail.com>
2024-10-19 23:32:29 -0300

    Change from csv to json

--- sketch.js
+++ sketch.js
@@ -2,57 +2,33 @@ let table_of_words;
 let words_json;
 let words;
 let input;
+let translation_input;
 let translation = "";
-
-let has_json = false;
 
 function preload() {
     words_json = loadJSON("/assets/words.json", loadJSONdata, loadFailed);
-    table_of_words = loadTable("/assets/words.csv", "header", loadData);
 }
 
 function loadFailed(error) {
-    has_json = false;
+    console.log("ERROR: " + error);
+    
 }
 
 function loadJSONdata(json) {
     words = [];
-    has_json = true;
 
     for (let word of json) {
         words.push(new Word(word.ses, word.eng, word.pos));
     }
-
-    console.log(words);
-}
-
-function loadData(table) {
-    if (has_json) return;
-
-    words = [];
-    let tableRows = table.getRows();
-    const delim = ",";
-
-    for (let row of tableRows) {
-        let ses = row.getString("solresol");
-        let eng = row.getString("english");
-        let pos = row.getString("part_of_speech");
-
-        let new_eng = split(eng, delim).map(str => str.trim());
-        words.push(new Word(ses, new_eng, pos));
-    }
-    
-    saveJSON(words, "/assets/words.json");
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    
-    input = createInput("solresol");
-    input.position(160, 160);
-    input.changed(changeInput);
+    noCanvas();
 
-    changeInput();
+    input = select("#txt");
+    translation_input = select("#translation");
+
+    input.input(changeInput);
 }
 
 function changeTranslation(value) {
@@ -64,11 +40,11 @@ function changeTranslation(value) {
     }
 
     if (found != undefined) {
-        translation += random(found.eng);
+        translation += found.eng[0]; //random(found.eng);
     } else {
-        translation += "<not found: ";
+        translation += "{not found: ";
         translation += value;
-        translation += ">";
+        translation += "}";
     }
     translation += " ";
 }
@@ -76,11 +52,15 @@ function changeTranslation(value) {
 function changeInput() {
     let values = splitTokens(input.value(), " ,.!?");
     translation = "";
-    console.log(values);
+    // console.log(values);
 
     for (let value of values) {
         changeTranslation(value);
     }
+
+    // console.log(translation);
+
+    translation_input.html(translation.trimEnd() + ".");
 }
 
 function draw() {
